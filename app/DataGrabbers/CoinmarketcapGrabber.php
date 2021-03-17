5cc4a76f5fbad7fca338f2add70b8a35271a47ce
Nariman <nngasanov@gmail.com>
2021-03-17 15:13:19 +0500

    Add the Coinmarketcap grabber

---
+++ app/DataGrabbers/CoinmarketcapGrabber.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\DataGrabbers;
+
+
+use Curl\Curl;
+
+class CoinmarketcapGrabber
+{
+    private $curl;
+    private $base_url = 'https://pro-api.coinmarketcap.com';
+    private $api_key = 'cfbc62ef-2570-4b52-84a2-8b12d271f1fe';
+
+    public function __construct()
+    {
+        $this->curl = new Curl();
+    }
+
+    public function get_coins_list() {
+        return $this->make_get_request('/v1/cryptocurrency/map', [
+            'listing_status' => 'active,untracked'
+        ]);
+    }
+
+    public function get_coins_data_by_id($ids = []) {
+        return $this->get_coins_data_by('id', $ids);
+    }
+
+    public function get_coins_data_by_slug($slugs = []) {
+        return $this->get_coins_data_by('slug', $slugs);
+    }
+
+    public function get_coins_data_by_symbol($symbols = []) {
+        return $this->get_coins_data_by('symbol', $symbols);
+    }
+
+    public function get_coins_data_by($by, $ids) {
+        return $this->make_get_request("/v1/cryptocurrency/info", [
+            $by => join(',', $ids)
+        ]);
+    }
+
+    private function make_get_request($endpoint, $data = []) {
+        $this->curl->setHeader('X-CMC_PRO_API_KEY', $this->api_key);
+        $this->curl->get($this->base_url . $endpoint, $data);
+        $response = $this->curl->getResponse();
+        $this->curl->reset();
+
+        return $response;
+    }
+}
