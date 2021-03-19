8ae0fc779dd17f04c53d85b1a2d65f1199aa7fe9
Nariman <nngasanov@gmail.com>
2021-03-20 02:26:05 +0500

    Init Tokens table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         (new CoinmarketcapDataSeeder())->run();
         (new CoingeckoDataSeeder())->run();
+        (new CollectDataToTokensSeeder())->run();
     }
 }
